c4720de4bf036aed2c7d562d8ca53f8bf588bacf
Mohammad Minul Islam <minul.islam@gmail.com>
2022-03-15 23:32:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ import pandas as pd
 ########### Define your variables ######
 
 # here's the list of possible columns to choose from.
-list_of_columns =['Cases - cumulative total', 'Deaths - cumulative total']
+list_of_columns =['Cases - cumulative total', 'Deaths - cumulative total',
+                   'Cases - newly reported in last 7 days','Deaths - newly reported in last 7 days']
 
-mycolumn='Cases - cumulative total'
-myheading1 = f"{mycolumn}!"
-mygraphtitle = 'COVID Cases by Country'
-mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
-mycolorbartitle = "Human Count"
+
 tabtitle = 'COVID 19'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
 githublink = 'https://github.com/minul-islam/301-old-mcdonald'
@@ -25,20 +22,6 @@ df = pd.read_csv('assets/WHO-COVID-19-global-table-data.csv')
 df.reset_index(inplace=True)
 df = df.rename(columns = {'index':'Country'})
 
-fig = go.Figure(data=go.Choropleth(
-    locations=df['Country'], # Spatial coordinates
-    z = df[mycolumn].astype(float), # Data to be color-coded
-    locationmode = 'country names', # set of locations match entries in `locations`
-    colorscale = mycolorscale,
-    colorbar_title = mycolorbartitle,
-))
-
-fig.update_layout(
-    title_text = mygraphtitle,
-    width=1200,
-    height=800
-)
-
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -48,16 +31,52 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1(myheading1),
-    dcc.Graph(
-        id='figure-1',
-        figure=fig
-    ),
+    html.H1('COVID-19 Cases & Death as of 15 Mar 2022'),
+    html.Div([
+        html.Div([
+                html.H6('Select a variable for analysis:'),
+                dcc.Dropdown(
+                    id='options-drop',
+                    options=[{'label': i, 'value': i} for i in list_of_columns],
+                    value='Cases - cumulative total'
+                ),
+        ], className='two columns'),
+        html.Div([dcc.Graph(id='figure-1'),
+            ], className='ten columns'),
+    ], className='twelve columns'),
     html.A('Code on Github', href=githublink),
     html.Br(),
     html.A("Data Source", href=sourceurl),
     ]
 )
+
+
+
+### define a function to create the chart 
+@app.callback(Output('figure-1', 'figure'),
+             [Input('options-drop', 'value')])
+
+def create_chart(var1): 
+    mygraphtitle = f'COVID {var1} by Country'
+    mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
+    mycolorbartitle = "Number of Person"
+
+    fig = go.Figure(data=go.Choropleth(
+        locations=df['Country'], # Spatial coordinates
+        z = df[var1].astype(float), # Data to be color-coded
+        locationmode = 'country names', # set of locations match entries in `locations`
+        colorscale = mycolorscale,
+        colorbar_title = mycolorbartitle,
+    ))
+
+    fig.update_layout(
+        title_text = mygraphtitle,
+        width=1200,
+        height=800
+    )
+
+    return fig
+
 
 ############ Deploy
 if __name__ == '__main__':
